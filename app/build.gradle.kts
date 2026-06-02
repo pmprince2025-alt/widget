75fbfe8650d27ec3aed28901c3ffadc781d255c5
Prince Meher <pmprince2025@gmail.com>
2026-06-02 12:36:21 +0530

    Add missing hilt-work dependency to app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,5 +57,7 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
     implementation(libs.hilt.navigation.compose)
+    implementation(libs.hilt.work)
+    implementation(libs.workmanager)
     implementation(libs.lifecycle.runtime.compose)
 }
